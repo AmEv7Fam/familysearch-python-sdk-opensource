67c283d7df699d51cad99c64a8373b2e751688e3
elderamevans <elderamevans@gmail.com>
2014-09-11 16:00:29 -0600

    Major Person bugfixing.

--- familysearch/person.py
+++ familysearch/person.py
@@ -4,7 +4,7 @@
 
 class Person:
     def __init__(self):
-        pass
+        self.person_base = self.tree_base + 'persons/'
         
     def create_person(self, data):
         pass # Working on it!
@@ -12,58 +12,58 @@ class Person:
         #response = self._request(url, data)    
         
     def parents(self, pid):
-        url = self.tree_base + pid + "/parents"
+        url = self.person_base + pid + "/parents"
         response = self._request(url)
         return response
     
     def spouses(self, pid):
-        url = self.tree_base + pid + "/spouses"
+        url = self.person_base + pid + "/spouses"
         response = self._request(url)
         return response
     
     def children(self, pid):
-        url = self.tree_base + pid + "/children"
+        url = self.person_base + pid + "/children"
         response = self._request(url)
         return response
     
     def memories_query(self, pid):
-        url = self.tree_base + 'persons/' + pid + '/memories'
+        url = self.person_base + pid + '/memories'
         response = self._request(url)
         response = self._fs2py(response)
         return response
     
     def person_portrait(self, pid):
-        url = tree_base + "persons/" + pid + 'portrait'
+        url = self.person_base + pid + '/portrait'
         response = self._request(url)
         response = self._fs2py(response)
         return response
         
     def person_portraits(self, pid):
-        url = tree_base + "persons/" + pid + 'portraits'
+        url = self.person_base + pid + '/portraits'
         response = self._request(url)
         response = self._fs2py(response)
         return response
     
     def spouse_relationships(self, pid):
-        url = tree_base + "persons/" + pid + '/spouse-relationships'
+        url = self.person_base + pid + '/spouse-relationships'
         response = self._request(url)
         response = self._fs2py(response)
         return response
     
     def child_relationships(self, pid):
-        url = tree_base + "persons/" + pid + 'child-relationships'
+        url = self.person_base + pid + '/child-relationships'
         response = self._request(url)
         response = self._fs2py(response)
         return response
     
     def parent_relationships(self, pid):
-        url = tree_base + "persons/" + pid + 'parent_relationships'
+        url = self.person_base + pid + '/parent_relationships'
         response = self._request(url)
         response = self._fs2py(response)
         return response
     
-    def persons_with_relationships(self):
-        url = tree_base + "persons-with-relationships.json?person=" + pid
+    def person_with_relationships(self):
+        url = self.tree_base + "persons-with-relationships?person=" + pid
         response = self._request(url)
         response = self._fs2py(response)
         return response
