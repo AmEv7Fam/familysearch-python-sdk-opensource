17f1cb23182d3ec037cbe69888d447705d4783a0
elderamevans <elderamevans@gmail.com>
2014-09-19 13:49:51 -0600

    Now we can remove a parent from a child-parent relationship!

--- familysearch/parentsAndChildren.py
+++ familysearch/parentsAndChildren.py
@@ -4,15 +4,17 @@
 
 class ParentsAndChildren:
     def __init__(self):
-        pass
+        self.child_base = self.tree_base + "child-and-parents-relationships/"
 
     def create_child_relationship(self, data):
         url = self.tree_base + 'relationships'
         response = self._request(url, data)
         return dict(response.info())
     
-    def cap_relationship_parent(self):
-        pass # TODO
+    def delete_child_relationship_parent(self, crid, parent):
+        url = self.child_base + crid + "/" + parent
+        response = self._request(url, method="DELETE")
+        return dict(response.info())
     
     def cap_relationship_conclusion(self):
         pass # TODO
