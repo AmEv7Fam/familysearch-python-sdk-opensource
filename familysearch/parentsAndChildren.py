ddb404e4c346e7c30e87e5d34d7dd27e9f0a0310
elderamevans <elderamevans@gmail.com>
2014-09-19 08:56:52 -0600

    Minor change to Create Child Relationship

--- familysearch/parentsAndChildren.py
+++ familysearch/parentsAndChildren.py
@@ -6,55 +6,8 @@ class ParentsAndChildren:
     def __init__(self):
         pass
 
-    def create_child_relationship(self, cpid, fpid='', mpid='', ftype='',
-                                  mtype='', reason=None):
+    def create_child_relationship(self, cpid, data):
         url = self.tree_base + 'relationships'
-        data = {
-            "childAndParentsRelationships" : [ {
-            "father" : {
-                "resource" : "https://familysearch.org/platform/tree/persons/" + mpid,
-                "resourceId" : mpid
-            },
-            "mother" : {
-                "resource" : "https://familysearch.org/platform/tree/persons/" + fpid,
-                "resourceId" : fpid
-            },
-              "child" : {
-                "resource" : self.person_base + cpid,
-                "resourceId" : cpid
-            },
-            "fatherFacts" : [ {
-                "id" : fpid,
-                "attribution" : {
-                    "contributor" : {
-                        "resource" : self.user_base + "/agents/" + self.current_user()['treeUserId']
-                    },
-                    "changeMessage" : reason
-                },
-                "type" : "http://gedcomx.org/" + ftype + "Parent"
-            } ],
-            "motherFacts" : [ {
-                "id" : mpid,
-                "attribution" : {
-                    "contributor" : {
-                        "resource" : self.user_base + "/agents/" + self.current_user()['treeUserId']
-                    },
-                    "changeMessage" : reason
-                },
-                "type" : "http://gedcomx.org/ "+ mtype + "Parent"
-              } ]
-            } ]
-            }
-        if not(mpid and fpid):
-            raise ValueError("Provide at least one parent to create a child-parent relationship.")
-        if not fpid:
-            del data['childAndParentsRelationships'][0]["father"]
-        if not ftype:
-            del data['childAndParentsRelationships'][0]['fatherFacts']
-        if not mpid:
-            del data['childAndParentsRelationships'][0]["mother"]
-        if not mtype:
-            del data['childAndParentsRelationships'][0]['motherFacts']
         response = self._request(url, data)
         return dict(response.info())
     
