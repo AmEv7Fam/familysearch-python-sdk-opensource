aa8536c33b5b860f9cf7bd56043bab450129bf1a
elderamevans <elderamevans@gmail.com>
2014-10-22 15:31:38 -0600

    Found some more bits.

--- familysearch/user.py
+++ familysearch/user.py
@@ -7,14 +7,12 @@ class User(object):
         pass
         
     def get_current_user(self):
-        return self.get(self.root_collection['collections'][0]['links']\
+        return self.get(self.root_collection['collections'][0]['links']
                         ['current-user']['href'])
     
     def get_current_user_person(self):
-        url = self.tree_base + "current-person"
-        response = self._request(url)
-        response = self._fs2py(response, 'persons')
-        return response[0]
+        return self.get(self.family_tree['collections'][0]['links']
+                        ['current-user-person']['href'])
     
     def get_agent(self, uid):
         url = self.user_base + "agents/" + uid
@@ -22,11 +20,9 @@ class User(object):
         response = self._fs2py(response, 'agents')
         return response[0]
     
-    def get_user_history(self):
-        url = self.user_base + "current/history"
-        response = self._request(url)
-        response = self._fs2py(response, 'sourceDescriptions')
-        return response[0]
+    def get_current_user_history(self):
+        return self.get(self.family_tree['collections'][0]['links']
+                        ['current-user-history']['href'])
 
 # FamilySearch imports
 
