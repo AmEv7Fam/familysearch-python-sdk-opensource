f3d9916c723362ed21e37cb70b0003b96d887356
elderamevans <elderamevans@gmail.com>
2014-10-22 14:32:09 -0600

    So, Discovery doesn't need us to be logged in to work... And signing in doesn't change the responses (as far as I can tell)...

--- familysearch/discovery.py
+++ familysearch/discovery.py
@@ -10,13 +10,29 @@ from . import FamilySearch
 class Discovery(object):
     def __init__(self):
         self.root_collection = self.get(self.base + '/.well-known/collection')
-        self.family_tree = self.get(self.root_collection['collections'][0]\
-        ['links']['family-tree']['href'])
+        self.collections = self.get(self.root_collection['collections'][0]
+                                    ['links']['subcollections']['href'])
+        self.family_tree = self.get(self.collections['collections'][0]['links']
+                                    ['self']['href'])
+        self.historical_records = self.get(self.collections['collections'][1]
+                                           ['links']['self']['href'])
+        self.user_sources = self.get(self.collections['collections'][2]['links']
+                                     ['self']['href'])
+        self.memories_collection = self.get(self.collections['collections'][3]
+                                            ['links']['self']['href'])
+        self.discussions_collection = self.get(self.collections['collections']
+                                               [4]['links']['self']['href'])
+        self.places_authority = self.get(self.collections['collections'][5]
+                                         ['links']['self']['href'])
+        self.dates_authority = self.get(self.collections['collections'][6]
+                                        ['links']['self']['href'])
+        self.uid = {}
         
     def fix_discovery(self):
-        self.root_collection = self.get(self.base + '/platform/collection')
-        self.family_tree = self.get(self.root_collection['collections'][0]\
-        ['links']['family-tree']['href'])
+        try:
+            self.uid = self.get_current_user()['users'][0]['treeUserId']
+        except KeyError:
+            self.uid = {}
     
     
 # FamilySearch hookup
