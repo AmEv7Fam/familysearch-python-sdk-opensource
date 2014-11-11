df9f0f316fc3815b0c4725a66b6dc9c6fd85ff4f
elderamevans <elderamevans@gmail.com>
2014-11-11 14:27:59 -0700

    Note to self: This is something where we want immutable default arguments!

--- familysearch/pedigree.py
+++ familysearch/pedigree.py
@@ -6,13 +6,13 @@ class Pedigree:
     def __init__(self):
         pass
         
-    def get_ancestry(self, pid, query, **kwargs):
+    def get_ancestry(self, pid, query="", **kwargs):
         return self.get(self._add_query_params(
-            self.tree_base + 'ancestry?person=' + pid), query, **kwargs)
+            self.tree_base + 'ancestry?person=' + pid, query, **kwargs))
     
-    def get_descendancy(self, pid, query, **kwargs):
+    def get_descendancy(self, pid, query="", **kwargs):
         return self.get(self._add_query_params(
-            self.tree_base + 'descendancy?person=' + pid), query, **kwargs)
+            self.tree_base + 'descendancy?person=' + pid, query, **kwargs))
 
 # FamilySearch imports
 
