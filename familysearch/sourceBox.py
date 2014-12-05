e8aed69ac3fe7fe588b977022b38a78d5d1f9e6d
elderamevans <elderamevans@gmail.com>
2014-12-05 15:16:08 -0700

    Can't believe Source Box evaded me somehow...
    I'll leave those for later. Those will be fun to name.

--- familysearch/sourceBox.py
+++ familysearch/sourceBox.py
@@ -4,22 +4,26 @@
 
 class SourceBox:
     def __init__(self):
-        pass # TODO
+        self.source_base = self.base + "/platform/sources/"
     
-    def ud_collections__user(self):
+    def read_specific_user_collections(self, uid):
         pass # TODO
+        return self.get(self.source_base + uid + "/collections")
     
-    def ud_collections(self):
-        pass # TODO
+    def post_user_collection(self, data):
+        return self.get(self.source_base + "collections", data, method="POST")
     
-    def ud_collection(self):
-        pass # TODO
+    def post_user_collection(self, udsid, data):
+        return self.get(self.source_base + "collections/" + uid, data, method="POST")
+        
+    def delete_user_collection(self, udsid, data):
+        return dict(self._request(self.source_base + "collections/" + uid, data, method="DELETE").items())
     
     def ud_collection_source_descriptions(self):
-        pass # TODO
+        pass # TODO https://familysearch.org/developers/docs/api/sources/User-Defined_Collection_Source_Descriptions_resource
     
     def ud_collections_source_descriptions_user(self):
-        pass # TODO
+        pass # TODO https://familysearch.org/developers/docs/api/sources/User-Defined_Collections_Source_Descriptions_for_a_User_resource
 
 # FamilySearch imports
 
