1e657647e5e78dee3836ab8262a2107f77f17976
elderamevans <elderamevans@gmail.com>
2015-02-14 13:44:50 -0700

    Let's just get #16 started.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'familysearch-python-sdk-opensource',
+  packages = ['familysearch-python-sdk-opensource'],
+  version = '0.75',
+  description = 'A Python SDK for FamilySearch.org',
+  author = 'Elder Evans',
+  author_email = 'elderamevans@gmail.com',
+  url = 'https://github.com/elderamevans/familysearch-python-sdk-opensource',
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', 
+  keywords = ['FamilySearch', 'API', 'REST', 'family history', 'geneaolgy', 'JSON'] #
+  classifiers = [],
+)
