abe47657b0189d6c31aba5aec83dee8d3e75bd21
Elder Evans <elderamevans@gmail.com>
2015-02-26 08:47:18 -0700

    Added prototype OEmbed function.
    Seriously, though, this is the third time I've fixed utilities. You'd think it would have stuck by now...

--- familysearch/utilities_.py
+++ familysearch/utilities_.py
@@ -4,19 +4,25 @@
 # Magic
 
 class Utilities:
+    """https://familysearch.org/developers/docs/api/resources#utilities"""
     def __init__(self):
-        """https://familysearch.org/developers/docs/api/resources#utilities"""
+        """https://familysearch.org/developers/docs/api/examples#utilities"""
         pass
 
-    def get_pending_modifications(self):
-        """Obsolete."""
+    def pending_modifications(self):
+        """https://familysearch.org/developers/docs/api/tree/Pending_Modifications_resource"""
         return self.get(self.root_collection['response']['collections'][0]['links']\
                         ['pending-modifications']['href'])
 
     def redirect(self, **kwargs):
-        """Obsolete."""
+        """https://familysearch.org/developers/docs/api/tree/Redirect_resource"""
         return self._add_query_params(
-          self.base + "platform/redirect", kwargs)
+            self.base + "platform/redirect", kwargs)
+          
+    def oembed(self, **kwargs):
+        """https://familysearch.org/developers/docs/api/discovery/OEmbed_resource"""
+        return self._add_query_params(
+            self.base + "platform/oembed", kwargs)
 
 from familysearch import FamilySearch
 FamilySearch.__bases__ += (Utilities,)
