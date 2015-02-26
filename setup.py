e9823e00d3a73daf7c5a53a0f4bd7f6bb2691e60
elderamevans <elderamevans@gmail.com>
2015-02-26 10:43:29 -0700

    That should be good enough for now...
    Let's close #16.

--- setup.py
+++ setup.py
@@ -1,14 +1,36 @@
 from distutils.core import setup
 
+import familysearch
+
 setup(
   name = 'familysearch-python-sdk-opensource',
   packages = ['familysearch-python-sdk-opensource'],
-  version = '0.75',
+  version = familysearch.__version__,
   description = 'A Python SDK for FamilySearch.org',
+  long_description=open('README.md').read()
   author = 'Elder Evans',
   author_email = 'elderamevans@gmail.com',
   url = 'https://github.com/elderamevans/familysearch-python-sdk-opensource',
   download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', 
   keywords = ['FamilySearch', 'API', 'REST', 'family history', 'geneaolgy', 'JSON'] #
-  classifiers = [],
+  classifiers = [
+    "Development Status :: 4 - Beta",
+    "Environment :: Console",
+    "Environment :: MacOS X",
+    "Environment :: Web Environment",
+    "Environment :: Win32 (MS Windows)",
+    "Environment :: X11 Applications",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.2",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Topic :: Internet :: WWW/HTTP",
+    "Topic :: Sociology :: Genealogy",
+    "Topic :: Software Development :: Libraries :: Python Modules"
+  ],
 )
