aa37f537fe8137f4d1b0063343da831cbe79e019
Elder Evans <elderamevans@gmail.com>
2015-03-30 13:25:59 -0600

    ...And a more permanent fix.

--- familysearch/authorities.py
+++ familysearch/authorities.py
@@ -11,5 +11,14 @@ class Authorities():
 
     def dates(self, **kwargs):
         """https://familysearch.org/developers/docs/api/dates/Date_resource."""
-        return self._add_query_params(
-            self.base + '/platform/dates', kwargs)
+        
+        try:
+            url = self.collections['FSDA']['response']['collections'][0][
+                'links']['normalized-date']['template']
+        except KeyError:
+            self.update_collection("FSDA")
+            url = self.collections['FSDA']['response']['collections'][0][
+                'links']['normalized-date']['template']
+        shim = {}
+        shim["?date,access_token"] = ""
+        return self._add_query_params(url.format(**shim), kwargs)
