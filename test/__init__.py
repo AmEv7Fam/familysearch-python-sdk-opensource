0bc8e4cd58f823fdea90d5d4778c8ca698d3c0cb
Elder Evans <elderamevans@gmail.com>
2015-04-03 10:44:31 -0600

    Still figuring this out... (crosses fingers...)

--- test/__init__.py
+++ test/__init__.py
@@ -1,19 +1,2 @@
 # -*- coding: utf-8 -*-
 """Make test folder a module."""
-
-from test.test_basefs import TestFamilySearch
-from test.test_enhanced_request import TestEnhancedRequest
-
-fs = TestFamilySearch()
-fs.setUp()
-fs.test_base_fs_creation()
-fs.tearDown()
-
-er = TestEnhancedRequest()
-er.setUp()
-er.test_delete()
-er.test_head()
-er.test_options()
-er.test_post()
-er.test_put()
-er.tearDown()
---
+++ test/run_tests.py
@@ -0,0 +1,16 @@
+from test.test_basefs import TestFamilySearch
+from test.test_enhanced_request import TestEnhancedRequest
+
+fs = TestFamilySearch()
+fs.setUp()
+fs.test_base_fs_creation()
+fs.tearDown()
+
+er = TestEnhancedRequest()
+er.setUp()
+er.test_delete()
+er.test_head()
+er.test_options()
+er.test_post()
+er.test_put()
+er.tearDown()
