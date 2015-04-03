706e3708ca3bb32968970916b6329ac97b5bc8db
Elder Evans <elderamevans@gmail.com>
2015-04-03 12:03:18 -0600

    Make that "T" not "_t".

--- test/run_tests.py
+++ test/run_tests.py
@@ -2,8 +2,8 @@ from test.test_basefs import TestFamilySearch
 #from test.test_enhanced_request import TestEnhancedRequest
 
 fs = TestFamilySearch()
-fs.run_tests()
+fs.runTests()
 
 # Removing until we can get it platform-independent...
 #er = TestEnhancedRequest()
-#er.run_tests()
+#er.runTests()
