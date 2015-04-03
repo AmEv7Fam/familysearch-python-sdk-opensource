7c1de9d149d5dbf8648d537a49c1ed14006c2b72
Elder Evans <elderamevans@gmail.com>
2015-04-03 12:18:58 -0600

    Another typo.

--- test/run_tests.py
+++ test/run_tests.py
@@ -2,7 +2,7 @@ from test.test_basefs import TestFamilySearch
 #from test.test_enhanced_request import TestEnhancedRequest
 
 fs = TestFamilySearch()
-fs.runTests()
+fs.runTest()
 
 # Removing until we can get it platform-independent...
 #er = TestEnhancedRequest()
